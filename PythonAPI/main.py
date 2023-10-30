1913937518328f6b9b9002b0e1399a5cd9440e79
peeb01 <kitisaiyot01@gmail.com>
2023-10-31 04:11:45 +0700

    NPiB

--- PythonAPI/main.py
+++ PythonAPI/main.py
@@ -179,6 +179,105 @@ async def massuertype(mtype: MassuerType):
     except:
         raise HTTPException(status_code=500, detail="Nothing.")
     
+#------------------------------------------------- Queue and next Time -------------------------------------------------------#
+class selectDay(BaseModel):
+    dateTimes : str
+    massuertype : str
+
+@app.post("/main/massuertype/selectnexttime")
+async def select_next_time(select_day: selectDay):
+    input_date_str = select_day.dateTimes
+    massuertype = select_day.massuertype
+
+    input_date = datetime.strptime(input_date_str, "%Y-%b-%d")
+
+    day_of_week = input_date.strftime("%A")
+    query = """
+            select M.masuerId, M.fname, M.lname
+            from Masuer M
+            where M.dayoff <> ? and M.massuertype = ? and M.statusNow = 1
+            """
+
+    cursor.execute(query, (day_of_week, massuertype))
+    result = cursor.fetchall()
+    dict_list = [{'ID': item[0],'First Name': item[1], 'Last Name': item[2]} for item in result]
+    results_json = json.dumps(dict_list, ensure_ascii=False, indent=3)
+    return results_json
+
+#-------------------------------------------- Buy, Booking -----------------------------------------#
+from datetime import timedelta
+
+class init_buy(BaseModel):
+    username : str
+    masuerId : int
+    masuerFName : str
+    masuerLName : str
+    timewant : int      # minutes
+    time_bookingwant : str
+
+
+@app.post("/main/massuertype/selectnexttime/book-appointment")
+async def book_appointment(booking_info: init_buy):
+
+    username = booking_info.username
+    masuerId = booking_info.masuerId
+    masuerFName = booking_info.masuerFName
+    masuerLName = booking_info.masuerLName
+    timewant = booking_info.timewant
+    timebookwill = datetime.strptime(booking_info.time_bookingwant, "%Y-%b-%d %H-%M-%S")
+    execute_cursor = cursor.execute("select * from Booking").fetchall()
+    bookingId = len(execute_cursor) + 1
+
+    points = timewant
+
+    current_time = datetime.now()
+    
+    if timebookwill <= current_time:
+        return {"message": "Booking time must be after the current time."}
+
+    timeout = timebookwill + timedelta(minutes=timewant)
+
+    try:
+        current_time_str = current_time.strftime('%Y-%m-%d %H:%M:%S')
+
+        # Check if there are any existing bookings with the same masuerId and overlapping time periods
+        conflict_query = """
+                        select count(*) from Booking
+                        where masuerID = ? and
+                              (Timeofout >= ? and Timemasuer <= ?)
+                        """
+        conflict_count = cursor.execute(conflict_query, (masuerId, timebookwill, timeout)).fetchone()[0]
+
+        if conflict_count > 0:
+            return {"message": "Booking conflicts with an existing appointment for the same masuerId."}
+
+        # Fetch the current points from the Customer table
+        points_query = "select points from Customer where username = ?"
+        current_points = cursor.execute(points_query, username).fetchone()[0]
+        
+        # Calculate new points
+        new_points = current_points + points
+        
+        # Update the points in the Customer table
+        update_point = """
+                update Customer
+                set points = ?
+                where username = ?
+                """
+        cursor.execute(update_point, (new_points, username))
+        
+        # Insert the booking into the database
+        query = """
+                insert into Booking (bookingId, username, masuerID, massuertype, datTime, Timemasuer, Timeofout)
+                values (?, ?, ?, ?, ?, ?, ?)
+                """
+        cursor.execute(query, (bookingId, username, masuerId, f"{masuerFName} {masuerLName}", current_time_str, timebookwill, timeout))
+        
+        conn.commit()
+
+        return {"message": "Appointment booked successfully"}
+    except Exception as e:
+        return {"message": f"Error: {str(e)}"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
