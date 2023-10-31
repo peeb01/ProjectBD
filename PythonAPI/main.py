f00a7df59ce477ce92976dd3eedab09e8b8464a5
peeb01 <kitisaiyot01@gmail.com>
2023-10-31 15:08:35 +0700

    NPiB

--- PythonAPI/main.py
+++ PythonAPI/main.py
@@ -240,7 +240,7 @@ async def book_appointment(booking_info: init_buy):
     try:
         current_time_str = current_time.strftime('%Y-%m-%d %H:%M:%S')
 
-        # Check if there are any existing bookings with the same masuerId and overlapping time periods
+        # Check if existing bookings with the same masuerId and overlapping time periods
         conflict_query = """
                         select count(*) from Booking
                         where masuerID = ? and
@@ -278,6 +278,35 @@ async def book_appointment(booking_info: init_buy):
         return {"message": "Appointment booked successfully"}
     except Exception as e:
         return {"message": f"Error: {str(e)}"}
+
+#--------------------------------------------------Massuer Scheduling------------------------------------------#
+class MassuerScheduiling(BaseModel):
+    massuerId : int
+
+@app.post("/main/massuerscheduling")
+def selectMasuerScheduling(massuer: MassuerScheduiling):
+    msuerId = massuer.massuerId
+
+    current_time = datetime.now()
+
+    query = """
+            select M.fname, M.lname, C.fname, B.Timemasuer
+            from Customer C, Masuer M, Booking B
+            where (B.username = C.username) and (M.masuerId = B.masuerID) and (B.masuerID = ?) and (B.Timemasuer >= ?)
+            """
+    cursor.execute(query, (msuerId, current_time))
+    result = cursor.fetchall()
+    dict_list = [{'Masuer firstname': item[0], 'Masuer lastname': item[1], 'Customer name': item[2], 'start Time': item[3]} for item in result]
+
+    # Convert datetime to strings
+    for item in dict_list:
+        item['start Time'] = item['start Time'].isoformat()
+
+    # Sort the results by 'start Time' 
+    sorted_dict_list = sorted(dict_list, key=lambda item: item['start Time'])
+
+    results_json = json.dumps(sorted_dict_list, ensure_ascii=False, indent=4)
+    return results_json
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
